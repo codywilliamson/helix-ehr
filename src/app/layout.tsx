510cc06b974cda1e5250221fd257fa4ec646156e
Cody Williamson <codywscw@gmail.com>
2026-03-24 23:13:33 -0500

    fix: middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="bg-background text-foreground flex min-h-full flex-col">
+      <body
+        className="bg-background text-foreground flex min-h-full flex-col"
+        suppressHydrationWarning
+      >
         <AuthSessionProvider>
           <ApolloProviderWrapper>
             {children}
